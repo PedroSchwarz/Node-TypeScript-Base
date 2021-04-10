32da68df7410e64eccae0b9e2933c84597537d7c
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2021-04-10 18:17:21 -0300

    Added eslint config file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
+// Imports
+import express from "express";
 // Get project port
 const PORT = 5000 || process.env.PORT;
-// Imports
-import express from 'express';
 const app = express();
 app.use(express.json());
 // Routes
-app.get('/', (req, res) => {
-    return res.json({message: 'Working'});
-});
+app.get("/", (req, res) => res.json({ message: "Working" }));
 // Start server
 app.listen(PORT);
