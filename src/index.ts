aca7ec40498c77a73fb836a9884a9dd752443df5
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2021-04-11 21:51:17 -0300

    Added prettier config file to use single quotes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Imports
-import express from "express";
+import express from 'express';
 // Get project port
 const PORT = 5000 || process.env.PORT;
 // Init express app
@@ -7,6 +7,6 @@ const app = express();
 // Set express to use json
 app.use(express.json());
 // Routes
-app.get("/", (req, res) => res.json({ message: "Working" }));
+app.get('/', (req, res) => res.json({ message: 'Working' }));
 // Start server
 app.listen(PORT);
